14233a64d946d1415838f63d580d1d999a849bb1
Mohamed Traore <mohamed.s.c.traore@gmail.com>
2019-04-20 23:02:11 +0000

    create front end scaffold

---
+++ front-end/tests/unit/Login.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mohamed on 4/20/19.
+ */
+import Vue from 'vue'
+import LoginPage from '@/views/LoginPage'
+
+describe('LoginPage.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(LoginPage)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .toEqual('TaskAgile')
+  })
+})
