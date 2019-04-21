09fcad737c0107c90c28de919d50c1dfd4231650
Mohamed Traore <mohamed.s.c.traore@gmail.com>
2019-04-21 12:30:40 +0000

    implement UI of register page

---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper">
+          <img class="logo" src="/static/images/logo.png">
+          <div class="tagline">Open source task management tool</div>
+        </div>
+          <form>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username">
+            </div>
+            <div class="form-group">
+              <label for="emailAddress">Email Address</label>
+              <input type="email" class="form-control" id="emailAddress">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Create account</button>
+          </form>
+        </div>
+      </div>
+      <footer class="footer">
+        <span class="copyright">&copy; 2019 Mohamed SC TRAORE</span>
+        <ul class="footer-links list-inline float-right">
+          <li class="list-inline-item"><a href="#">About</a></li>
+          <li class="list-inline-item"><a href="#">Terms of Service</a></li>
+          <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+          <li class="list-inline-item"><a href="https://github.com/taskagile/vuejs.spring-boot.mysql" target="_blank">GitHub</a></li>
+        </ul>
+      </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {max-width: 900px;}
+  .register-form {margin-top: 50px; max-width: 320px;}
+  .logo-wrapper {margin-bottom: 40px;}
+  .footer {width: 100%; line-height: 40px; margin-top: 50px; }
+</style>
